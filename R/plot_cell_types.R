0e85724dd8475d4a6bf278b3e5934d60275f499a
vgrozd <veselin.d.grozdanov@gmail.com>
2023-04-13 16:04:32 +0200

    plot_cell_types bugfix

--- R/plot_cell_types.R
+++ R/plot_cell_types.R
@@ -10,86 +10,91 @@
 #'@export
 
 
-plot_cell_types <- function(object, reduction="umap", idents){
-  .pal = rev(as.character(pals::glasbey(length(levels(factor(object[[idents]][,1]))))))
-  names(.pal) = rev(as.character(levels(factor(object[[idents]][,1]))))
+plot_cell_types <- function(object, idents=NULL, reduction="umap"){
+
+  df = if(!is.null(idents)) object[[idents]][,1] else Seurat::Idents(object)
+  if(length(unique(df)) >16) stop("Too many categories! Max. 16 cell types supported. ")
+  .pal <- pals::glasbey(length(unique(df))+5)[-c(1:5)]
+  names(.pal) = as.character(levels(factor(df)))
+
+
   print(
+
 
     Seurat::DimPlot(
       object,
       reduction=reduction,
-      group.by=idents,
+      group.by=if(!is.null(idents))idents,
       label = TRUE,
       repel=TRUE
     ) +
-    ggplot2::scale_color_manual(values=.pal) +
-    ggplot2::theme(
-      axis.text=element_text(size=12, color="black", face="bold.italic")
-    ) +
-    Seurat::NoLegend() +
-    ggplot2::xlab("") +
-    ggplot2::ylab("") +
-    ggplot2::ggtitle("") +
+      {if(!is.null(.pal)) ggplot2::scale_color_manual(values=.pal)} +
+      ggplot2::theme(
+        axis.text=element_text(size=12, color="black", face="bold.italic")
+      ) +
+      Seurat::NoLegend() +
+      ggplot2::xlab("") +
+      ggplot2::ylab("") +
+      ggplot2::ggtitle("") +
 
 
-
-    object[[idents]][,1] |>
+      df |>
       table() |>
       sort() |>
       as.data.frame() |>
       ggplot2::ggplot() +
-        ggplot2::aes(x="", y=Freq/sum(Freq)*100, fill=Var1) +
-        ggplot2::geom_bar(position="stack", stat="identity") +
-        ggplot2::geom_text(
-          ggplot2::aes(label=round(Freq/sum(Freq)*100)),
-            position=position_stack(vjust=0.5),
-            size=5,
-            fontface="bold.italic",
-            col="white",
-            hjust=0.5
-          ) +
-        ggplot2::scale_y_continuous(expand=c(0, 0), labels=c("0", "25 %", "50 %", "75 %", "100 %")) +
-        ggplot2::scale_fill_manual(values=.pal) +
-        ggplot2::xlab("") +
-        ggplot2::ylab("")  +
-        ggplot2::theme_classic() +
-        ggplot2::theme(
-          axis.text.y = element_text(
+      ggplot2::aes(x="", y=Freq/sum(Freq)*100, fill=df) +
+      ggplot2::geom_bar(position="stack", stat="identity") +
+      ggplot2::geom_text(
+        ggplot2::aes(label=round(Freq/sum(Freq)*100)),
+        position=position_stack(vjust=0.5),
+        size=5,
+        fontface="bold.italic",
+        col="white",
+        hjust=0.5
+      ) +
+      ggplot2::scale_y_continuous(expand=c(0, 0), labels=c("0", "25 %", "50 %", "75 %", "100 %")) +
+      {if(!is.null(.pal)) ggplot2::scale_fill_manual(values=.pal)} +
+      ggplot2::xlab("") +
+      ggplot2::ylab("")  +
+      ggplot2::theme_classic() +
+      ggplot2::theme(
+        axis.text.y = element_text(
           size=12,
           color="black",
           face="bold.italic"
-          ),
-          axis.ticks.x = element_blank()
-          ) +
-        Seurat::NoLegend() +
+        ),
+        axis.ticks.x = element_blank()
+      ) +
+      Seurat::NoLegend() +
 
 
-    object[[idents]][,1] |>
+      df |>
       table() |>
       sort() |>
       as.data.frame() |>
       ggplot2::ggplot() +
-        ggplot2::aes(x="", y=Freq/sum(Freq)*100, fill=Var1) +
-        ggplot2::geom_bar(position="stack", stat="identity") +
-        ggplot2::geom_text(
-          ggplot2::aes(label=Var1),
-          position=ggpp::position_stacknudge(vjust=0.5, x=-5),
-          size=4,
-          fontface="bold.italic",
-          col="black",
-          hjust=0
-          ) +
-        ggplot2::scale_y_continuous(expand=c(0, 0)) +
-        ggplot2::scale_fill_manual(values=rep("white", 14)) +
-        ggplot2::xlab("") +
-        ggplot2::ylab("")  +
-        ggplot2::theme_classic() +
-        ggplot2::theme(
-          axis.line = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text = element_blank()
-        ) +
-        Seurat::NoLegend() +
+      ggplot2::aes(x="", y=Freq/sum(Freq)*100, fill=df) +
+      ggplot2::geom_bar(position="stack", stat="identity") +
+      ggplot2::geom_text(
+        ggplot2::aes(label=df),
+        position=ggpp::position_stacknudge(vjust=0.5, x=-5),
+        size=4,
+        fontface="bold.italic",
+        col="black",
+        hjust=0
+      ) +
+      ggplot2::scale_y_continuous(expand=c(0, 0)) +
+      ggplot2::scale_fill_manual(values=rep("white", length(unique(df)))) +
+      ggplot2::xlab("") +
+      ggplot2::ylab("")  +
+      ggplot2::theme_classic() +
+      ggplot2::theme(
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank()
+      ) +
+      Seurat::NoLegend() +
 
       patchwork::plot_layout(widths = c(5,1,2))
 
